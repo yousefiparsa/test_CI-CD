f2567f47b5c0e0e126d8d0dd9e7cfa89ddbbe358
Parsa Yousefi <parsa@filestack.com>
2019-03-22 12:53:43 -0500

    production/staging mode

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-gunicorn app:app -w 4 -b 0.0.0.0:5005
+# gunicorn app:app -w 4 -b 0.0.0.0:5005
+
+echo "Selected environment: $1"
+
+if [ "$1" == "staging"]; then
+    echo "Deployment on Staging is successful!"
+else
+    echo "Deployment on Production is successful!"
+fi
